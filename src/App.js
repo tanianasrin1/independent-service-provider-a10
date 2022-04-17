39a6dd1f0562597285dae4881beadeec2516398c
tanianasrin1 <tasrineva169@gmail.com>
2022-04-17 18:17:21 +0600

    login register toggle fixed

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Footer from './pages/Shared/Footer/Footer';
 import Header from './pages/Shared/Header/Header';
 import Login from './pages/Login/Login/Login';
 import NotFound from './pages/Shared/NotFound/NotFound';
+import Register from './pages/Login/Register/Register';
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
         <Route path = "/home" element= {<Home></Home>}></Route>
         <Route path = "/about" element = {<About></About>}></Route>
         <Route path = "/login" element = {<Login></Login>}></Route>
+        <Route path = "/register" element = {<Register></Register>}></Route>
         <Route path = "*" element = {<NotFound></NotFound>}></Route>
       </Routes>
      <Footer></Footer>
---
+++ src/pages/Login/Register/Register.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Register.css'
+
+const Register = () => {
+    const navigate = useNavigate();
+
+    const navigateLogin = () =>{
+        navigate('/login');
+    }
+ 
+    const handleRegister = event =>{
+        event.preventDefault();
+    }
+
+    return (
+        <div className='register-form mt-4'>
+            <h2 style={{textAlign: 'center'}} className="register-title">Please Register!!!</h2>
+            <form className='mt-3'onSubmit={handleRegister} >
+                <input type="text" name="name" id="" placeholder='Your name' />
+                
+                <input type="email" name="email" id="" placeholder='Your email' required/>
+                
+                <input type="password" name="password" id=""  placeholder='Your password' required/>
+                <input type="submit" value="Register" />
+            </form>
+            <p>Already have an account? <Link to="/login" className="text-danger pe-auto text-decoration-none" onClick={navigateLogin}>Please Login</Link> </p>
+        </div>
+    );
+};
+
+export default Register;

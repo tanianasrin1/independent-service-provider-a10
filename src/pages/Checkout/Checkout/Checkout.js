9c34bc8143f1440ab8e2b45b499759c398b61393
tanianasrin1 <tasrineva169@gmail.com>
2022-04-18 18:12:08 +0600

    about me page fixed

--- src/pages/Checkout/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout/Checkout.js
@@ -3,8 +3,27 @@ import './Checkout.css'
 
 const Checkout = () => {
     return (
-        <div className='container text-center mt-4'>
-            <h2 className='checkout-title'>Please Checkout!!!</h2>
+        <div className='container  mt-4'>
+            <h2 className='checkout-title text-center'>Please Checkout!!!</h2>
+            <div>
+                <div >
+                    <h3>Happy Client</h3>
+                </div>
+
+              <div className='clients'>
+              <div className='client1 m-3 p-3'>
+                <p className="text-center mt-5"><small>Researching WordPress travel blogs is certainly an interesting and important first step when creating  travel site</small></p>
+                </div>
+                <div className='client2 m-3 p-3'>
+                <p className="text-center mt-5"><small>Researching WordPress travel blogs is certainly an interesting and important first step when creating  travel site</small></p>
+                </div>
+                <div className='client3 m-3 p-3'>
+                <p className="text-center mt-5"><small>Researching WordPress travel blogs is certainly an interesting and important first step when creating  travel site</small></p>
+                </div>
+              </div>
+                
+            </div>
+              
         </div>
     );
 };

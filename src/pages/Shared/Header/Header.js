2dcdd8b3426493cf191d1fd8a9ce955f924acb57
tanianasrin1 <tasrineva169@gmail.com>
2022-04-18 19:34:41 +0600

    readme fole fixed

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -23,13 +23,13 @@ const Header = () => {
     <Nav className="me-auto">
       <Nav.Link href="home#Services">Services</Nav.Link>
       <Nav.Link href="home#ExtraPart">ExtraPart</Nav.Link>
-      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+      {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
         <NavDropdown.Divider />
         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
+      </NavDropdown> */}
     </Nav>
     <Nav>
       <Nav.Link as={Link} to="/">Home</Nav.Link>

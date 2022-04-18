882cfeb04e57e1b504fdd3cbfe03703789f8d070
tanianasrin1 <tasrineva169@gmail.com>
2022-04-18 20:01:41 +0600

    readme file fixed

--- src/pages/About/AboutMe/AboutMe.js
+++ src/pages/About/AboutMe/AboutMe.js
@@ -13,7 +13,31 @@ const AboutMe = () => {
                 <br />
                 Name: Tania Nasrin Eva
                 <br />
-                <strong>Future Goals:</strong> Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quaerat sed velit modi, veniam rerum exercitationem dolor illo! Ipsum, ratione?
+                <strong>Future Goals:</strong> <p>At this stage setting goals is not hard but setting goals that make me feel motivated to achieve them is that is hard. The next step in my career which I think about- </p>
+                <ol>
+                    <li>Developing my personal brand: Developing my own brand helps me to have opportunities and tech companies reaching out of me. </li>
+                    <li>Find best team : Technical talent alone doesn’t offer satisfaction within a team and other qualities are needed. Finding the best team that has the same professional goals as is essential in achieving  goals.Need to have chemistry, trust, conflict resolution and communication to achieve my goals</li>
+                    <li>Learn a new technology: Learning new technologies can keep me aware of recent trends in technology and increase my skill set. </li>
+                    <li>Improve soft skills: A developer need to have strong programming and coding skills but it's also important to have strong soft skills to advance in the workplace. Soft skills can be things like:
+
+                                 <ul>
+                                      <li>Communication </li>
+                                      <li>Problem-solving</li>
+                                      <li>Creativity</li>
+                                      <li>Adaptability</li>
+                                      <li>Work ethic</li>
+                                      <li>Time management</li>
+
+                               </ul>
+
+                    </li>
+                    <li>Get a master's degree: A master's degree is an advanced degree in a specialty subject. A master's degree shows my additional expertise.</li>
+                    <li>Increase professional network: A developer can increase their professional network by meeting other people in their field.By  attending conferences, organize meetings and use social media help to build this network.</li>
+                    <li>Complete a coding project: A coding project can be new software or a new section of code that improves to understanding my abilities.</li>
+                    <li>Complete a system design:It can be good demonstration of my skills and understanding of the components of an efficient system.</li>
+                    <li>Become an expert in a domain: This can be a specific software, a programming language or a type of technology. As an expert, it may become a sought-after professional which is consult on projects or work for companies to develop interesting projects based on expertise. </li>
+                    <li>Give a presentation: Giving a presentation can help a me to increase  notoriety. Giving presentations is a good way to show a developer's expertise and network with people who positively impact in career.</li>
+                </ol>
 
             </div>
         </div>
--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
 import { signOut } from 'firebase/auth';
 
-// import logo from '../../../images/logo/p1.png'
+
 
 const Header = () => {
   const [user] = useAuthState(auth);
@@ -23,13 +23,7 @@ const Header = () => {
     <Nav className="me-auto">
       <Nav.Link href="home#Services">Services</Nav.Link>
       <Nav.Link href="home#ExtraPart">ExtraPart</Nav.Link>
-      {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown> */}
+     
     </Nav>
     <Nav>
       <Nav.Link as={Link} to="/">Home</Nav.Link>
